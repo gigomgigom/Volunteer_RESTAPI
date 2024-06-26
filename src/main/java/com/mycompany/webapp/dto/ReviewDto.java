68cc5ed251af4f6a5f0f9ae0a24658d4019dbf85
java_jth <zc6171@gmail.com>
2024-06-26 15:54:31 +0900

    dto field add

--- src/main/java/com/mycompany/webapp/dto/ReviewDto.java
+++ src/main/java/com/mycompany/webapp/dto/ReviewDto.java
@@ -1,8 +1,22 @@
 package com.mycompany.webapp.dto;
 
+import java.util.Date;
 import lombok.Data;
 
 @Data
 public class ReviewDto {
+	private int boardNo;
+	private int programNo;
+	private String memberId;
+	private String title;
+	private String content;
+	private int hitCnt;
+	private Date boardDate;
+	private String battachOname;
+	private byte battachData;
+	private String battachType;
+	private String imgOname;
+	private byte imgData;
+	private String imgType;
 
 }
--- src/main/java/com/mycompany/webapp/dto/VolSttsCtgDto.java
+++ src/main/java/com/mycompany/webapp/dto/VolSttsCtgDto.java
@@ -4,5 +4,6 @@ import lombok.Data;
 
 @Data
 public class VolSttsCtgDto {
-
+	private int sttsNo;
+	private String sttsName;
 }
