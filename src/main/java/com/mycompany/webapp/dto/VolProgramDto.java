e417c7e3b82bb8192aa2224498c38f9ad48bf955
gigomgigom <100766892+gigomgigom@users.noreply.github.com>
2024-07-01 16:52:46 +0900

    Add VolProgram edited

--- src/main/java/com/mycompany/webapp/dto/VolProgramDto.java
+++ src/main/java/com/mycompany/webapp/dto/VolProgramDto.java
@@ -11,39 +11,42 @@ import lombok.Data;
 
 @Data
 public class VolProgramDto {
-	private int programNo;
-	private String programTitle;
-	private int recruitStts;
+	private int programNo; //봉사프로그램 번호
+	private String programTitle; //봉사프로그램 제목
+	private int recruitStts; //모집 상태
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date actBgnDate;
+	private Date actBgnDate; //봉사 시작일자
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date actEndDate;
-	private int actBgnTime;
-	private int actEndTime;
+	private Date actEndDate; //봉사 종료일자
+	private int actBgnTime; //봉사 시작시간
+	private int actEndTime; //봉사 종료시간
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date recruitBgnDate;
+	private Date recruitBgnDate; //모집 시작일자
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private Date recruitEndDate;
-	private int recruitCnt;
-	private int applyCnt;
-	private String programCtg;
-	private int adultPosbl;
-	private int teenPosbl;
-	private String recruitName;
-	private String actPlace;
-	private String mngName;
-	private String mngTel;
-	private String regionNo;
-	private String battachOname;
-	private byte[] battachData;
-	private String battachType;
-	private String imgOname;
-	private byte[] imgData;
-	private String imgType;
-	private String location;
-	private int enabled;
-	private String content;
-	private MultipartFile battachFile;
-	private MultipartFile battachImg;
+	private Date recruitEndDate; //모집 종료일자
+	private int recruitCnt; //모집인원
+	private int applyCnt; //신청인원
+	private String programCtg; //봉사 분야코드
+	private int adultPosbl; //성인가능여부
+	private int teenPosbl; //청소년가능여부
+	private String recruitName; //모집기관
+	private String actPlace; //봉사장소
+	private String mngName; //담당자이름
+	private String mngTel; //담당자전화번호
+	private String regionNo; //군/구코드
+	
+	private String battachOname; //첨부파일 이름
+	private byte[] battachData; //첨부파일 데이터
+	private String battachType; //첨부파일 타입
+	private String imgOname; //이미지 이름
+	private byte[] imgData; //이미지 데이터
+	private String imgType; //이미지 타입
+	
+	private String location; //장소 (카카오맵 검색용도)
+	private int enabled; //봉사프로그램 활성/비활성화
+	private String content; //봉사 프로그램 내용
+	
+	private MultipartFile battachFile; //첨부파일 받는 multipartfile (Request 용도)
+	private MultipartFile battachImg; //이미지파일 받는 multipartfile (Request 용도)
 	
 }
