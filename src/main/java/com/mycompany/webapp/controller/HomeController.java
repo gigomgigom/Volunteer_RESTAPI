10997cdb54a0ab1a1df74fe3d2e99f97af1e9d07
gigomgigom <100766892+gigomgigom@users.noreply.github.com>
2024-07-01 14:55:56 +0900

    CTG data setted

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -1,17 +1,10 @@
 package com.mycompany.webapp.controller;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.webapp.service.AdminService;
 
 @RestController
 @RequestMapping("")
@@ -21,5 +14,5 @@ public class HomeController {
 	public String main() {
 		return "main";
 	}
-
+	
 }
--- src/main/java/com/mycompany/webapp/service/AdminService.java
+++ src/main/java/com/mycompany/webapp/service/AdminService.java
@@ -1,11 +1,15 @@
 package com.mycompany.webapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mycompany.webapp.dao.LikeUnlikeDao;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class AdminService {
+	
 
 }
